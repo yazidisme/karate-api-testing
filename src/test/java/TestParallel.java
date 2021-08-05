2617e0b64f2fbe40974a406cd8f5ce578447187a
Muhammad Yazid <muhammadyazid26@gmail.com>
2021-08-05 13:00:08 +0700

    Generate report using cucumber-reporting

--- src/test/java/TestParallel.java
+++ src/test/java/TestParallel.java
@@ -1,6 +1,14 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,7 +16,20 @@ public class TestParallel {
 
     @Test
     public void runTestParallel() {
-        Results results = Runner.path("classpath:users", "classpath:posts").tags("~@ignore").parallel(5);
+        Results results = Runner.path("classpath:users", "classpath:posts")
+                .outputCucumberJson(true)
+                .tags("~@ignore")
+                .parallel(5);
+        generateReport(results.getReportDir());
         assertEquals(results.getErrorMessages(), 0, results.getFailCount());
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "src/test/java");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 }
