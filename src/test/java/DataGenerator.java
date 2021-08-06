5554886ef89710ea016566b260a1c2fc2bc28e77
Muhammad Yazid <muhammadyazid26@gmail.com>
2021-08-06 16:56:07 +0700

    Call reusable feature for getting id from random existing id-data

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -1,5 +1,7 @@
 import com.github.javafaker.Faker;
 
+import java.util.Random;
+
 public class DataGenerator {
 
     private static final Faker faker = new Faker();
@@ -33,5 +35,11 @@ public class DataGenerator {
         return faker
                 .internet()
                 .emailAddress();
+    }
+
+    public static int pickRandomInt(int[] value) {
+        Random random = new Random();
+        int pickValue = random.nextInt(value.length);
+        return value[pickValue];
     }
 }
---
+++ src/test/java/users/user-reusable.feature
@@ -0,0 +1,13 @@
+@ignore @report=false
+Feature: User reusable scenario
+
+    Background:
+        Given url urlBase + 'users'
+
+    Scenario: Get random user id from existing data
+        When method get
+        Then status 200
+          * def jsonPath = function(arg) { return karate.jsonPath(arg, '$[*].id') }
+          * def res = call jsonPath response
+          * def testData = Java.type('DataGenerator')
+          * def id = testData.pickRandomInt(res)
