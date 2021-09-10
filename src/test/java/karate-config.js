1e50ad5c88041453e1d51564e24583dfbed51b03
Yazid <muhammadyazid26@gmail.com>
2021-09-10 18:31:48 +0700

    add another karate global config

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -5,5 +5,11 @@ function fn() {
     };
     karate.configure('connectTimeout', 5000);
     karate.configure('readTimeout', 5000);
+    /**
+        remove all karate config from all features, then uncomment code below
+    **/
+//    karate.configure('logPrettyRequest', true);
+//    karate.configure('logPrettyResponse', true);
+//    karate.configure('report', { showLog: true, showAllSteps: false });
     return config;
 }
