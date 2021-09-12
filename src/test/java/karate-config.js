385f4757e91513a615d4a9a2bd55a5edd1da9c13
Yazid <muhammadyazid26@gmail.com>
2021-09-12 22:16:01 +0700

    Scenario with callonce and afterScenario, also for global config

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -5,11 +5,19 @@ function fn() {
     };
     karate.configure('connectTimeout', 5000);
     karate.configure('readTimeout', 5000);
+
     /**
-        remove all karate config from all features, then uncomment code below
+        remove all karate config from all features, then uncomment code below for another global config
     **/
 //    karate.configure('logPrettyRequest', true);
 //    karate.configure('logPrettyResponse', true);
 //    karate.configure('report', { showLog: true, showAllSteps: false });
+
+    /**
+        set the feature file in the karate.call() function for global afterFeature and or afterScenario
+    **/
+//    karate.configure('afterFeature', function() { karate.call('YOUR_FEATURE_FILE') });
+//    karate.configure('afterScenario', function() { karate.call('YOUR_FEATURE_FILE') });
+
     return config;
 }
